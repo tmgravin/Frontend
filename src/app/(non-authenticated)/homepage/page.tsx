a223ff83e4e4d3785149750fe9b16bd05f37cc49
Ghost <b0677908@gmail.com>
2024-07-18 12:06:10 +0545

    project initialized

---
+++ src/app/(non-authenticated)/homepage/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Homepage from '@/components/non-authenticted/Homepage'
+
+function page() {
+  return (
+    <div>
+        <Homepage/>
+    </div>
+  )
+}
+
+export default page
