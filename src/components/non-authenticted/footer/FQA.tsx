806641ef3b0bc11b782d125e375b6ed8ad6c8c8c
bishnuf66 <bkbishnu2000@gmail.com>
2024-08-29 10:31:46 +0545

    changed assginments to proejcts from faqs

--- src/components/non-authenticted/footer/FQA.tsx
+++ src/components/non-authenticted/footer/FQA.tsx
@@ -20,7 +20,7 @@ function FQA() {
       <div className="space-y-6">
         <div>
           <h2
-            className="text-lg  cursor-pointer mb-2 flex items-center justify-between"
+            className="text-lg  font-semibold  cursor-pointer mb-2 flex items-center justify-between"
             onClick={() => toggleQuestion(1)}
           >
             <span>1. What is MSP Academy?</span>
@@ -33,11 +33,10 @@ function FQA() {
           </h2>
           {openQuestion === 1 && (
             <p className="pl-4 text-gray-700">
-              MSP Academy is a platform designed to connect Assignment Creators,
-              who need academic assignments completed, with Assignment Doers,
-              who complete these assignments. The platform is overseen by an
-              Admin who ensures smooth operations and compliance with our
-              policies.
+              MSP Academy is a platform designed to connect Creators, who need
+              academic projects completed, with Doers, who complete these
+              projects. The platform is overseen by an Admin who ensures smooth
+              operations and compliance with our policies.
             </p>
           )}
         </div>
@@ -58,14 +57,12 @@ function FQA() {
           {openQuestion === 2 && (
             <ul className="pl-4 text-gray-700 list-disc">
               <li>
-                <strong>Assignment Creator:</strong> A user who submits
-                assignment requests, providing specific details and requirements
-                for the task.
+                <strong>Creator:</strong> A user who submits project requests,
+                providing specific details and requirements for the task.
               </li>
               <li>
-                <strong>Assignment Doer:</strong> A user who accepts and
-                completes assignments based on the instructions provided by the
-                Assignment Creator.
+                <strong>Doer:</strong> A user who accepts and completes projects
+                based on the instructions provided by the creator.
               </li>
             </ul>
           )}
@@ -76,9 +73,7 @@ function FQA() {
             className="text-lg font-semibold cursor-pointer mb-2 flex items-center justify-between"
             onClick={() => toggleQuestion(3)}
           >
-            <span>
-              3. How do I register as an Assignment Creator or Assignment Doer?
-            </span>
+            <span>3. How do I register as an Creator or Doer?</span>
             <FontAwesomeIcon
               icon={faChevronDown}
               className={`transition-transform duration-300 ${
@@ -89,9 +84,9 @@ function FQA() {
           {openQuestion === 3 && (
             <p className="pl-4 text-gray-700">
               To register, simply visit our registration page, select your role
-              (Assignment Creator or Assignment Doer), and fill out the required
-              information. Once your registration is complete, you’ll be able to
-              start using the platform.
+              (Creator or Doer), and fill out the required information. Once
+              your registration is complete, you’ll be able to start using the
+              platform.
             </p>
           )}
         </div>
@@ -101,7 +96,7 @@ function FQA() {
             className="text-lg font-semibold cursor-pointer mb-2 flex items-center justify-between"
             onClick={() => toggleQuestion(4)}
           >
-            <span>4. How does the assignment process work?</span>
+            <span>4. How does the as project process work?</span>
             <FontAwesomeIcon
               icon={faChevronDown}
               className={`transition-transform duration-300 ${
@@ -111,28 +106,26 @@ function FQA() {
           </h2>
           {openQuestion === 4 && (
             <div className="pl-4 text-gray-700">
-              <h3 className="font-semibold mb-1">For Assignment Creators:</h3>
+              <h3 className="font-semibold mb-1">For Creators:</h3>
               <ul className="list-disc mb-4">
                 <li>
-                  Submit an assignment request with all necessary details and
+                  Submit an project request with all necessary details and
                   instructions.
                 </li>
                 <li>
-                  Review completed assignments and request revisions if
-                  necessary.
+                  Review completed projectss and request revisions if necessary.
                 </li>
                 <li>Approve the final submission and provide payment.</li>
               </ul>
 
-              <h3 className="font-semibold mb-1">For Assignment Doers:</h3>
+              <h3 className="font-semibold mb-1">For Doers:</h3>
               <ul className="list-disc">
                 <li>
-                  Browse available assignments and choose one that matches your
+                  Browse available projects and choose one that matches your
                   skills.
                 </li>
                 <li>
-                  Complete the assignment according to the provided
-                  instructions.
+                  Complete the projects according to the provided instructions.
                 </li>
                 <li>
                   Submit the completed work for review and receive payment once
@@ -158,10 +151,10 @@ function FQA() {
           </h2>
           {openQuestion === 5 && (
             <p className="pl-4 text-gray-700">
-              Payments are processed through the platform. Assignment Creators
-              deposit the agreed amount when submitting an assignment request.
-              Once the Assignment Doer completes and the Creator approves the
-              work, the payment is released to the Doer.
+              Payments are processed through the platform. Creators deposit the
+              agreed amount when submitting an project request. Once the Doer
+              completes and the Creator approves the work, the payment is
+              released to the Doer.
             </p>
           )}
         </div>
@@ -182,7 +175,7 @@ function FQA() {
           {openQuestion === 6 && (
             <p className="pl-4 text-gray-700">
               We prioritize the security and confidentiality of your data. All
-              personal information and assignment details are protected by
+              personal information and project details are protected by
               encryption and handled in accordance with our Privacy Policy.
             </p>
           )}
