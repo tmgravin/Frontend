59a2d19d3225ee307b2db41488301670ca1da920
Kshitiz <kshitizbabik.37@gmail.com>
2024-09-10 15:54:06 +0545

    Fix Build Issue in Layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import Script from "next/script";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ProjectProvider } from "@/components/providers/ProjectProvider";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { cn } from "@/lib/utils";
 
 const inter = Poppins({ weight: "500", subsets: ["latin"] });
@@ -25,9 +24,6 @@ export default function RootLayout({
       <Script src="https://kit.fontawesome.com/deb7916e2f.js"></Script>
       <Script src="https://accounts.google.com/gsi/client"></Script>
 
-      <GoogleOAuthProvider
-        clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}
-      >
         <ProjectProvider>
           <body
             className={cn(inter.className, "antialiased overflow-x-hidden")}
@@ -36,7 +32,6 @@ export default function RootLayout({
             <ToastContainer />
           </body>
         </ProjectProvider>
-      </GoogleOAuthProvider>
     </html>
   );
 }
